bb95ed82515962925ef3459281f6031dc3b244d5
Ross-Morgan <morgan.ross.512@gmail.com>
2023-04-21 22:49:01 +0100

    make roots actually work

--- src/roots.rs
+++ src/roots.rs
@@ -3,11 +3,17 @@ use core::f64::consts::PI;
 
 use num_complex::Complex64;
 
+pub fn real_root(n: f64, nth_root: usize) -> f64 {
+    round(n.powf(1.0 / (nth_root as f64)), 10)
+}
+
 /// Returns nth-roots of n
 pub fn root(n: f64, nth_root: usize) -> Vec<Complex64> {
+    let real = real_root(n, nth_root);
+
     (0..nth_root).map(|idx| {
         let theta = 2.0 * (idx as f64) / (nth_root as f64) * PI;
-        Complex64::from_polar(n, theta)
+        Complex64::from_polar(real, theta)
     }).collect()
 }
 
@@ -30,6 +36,10 @@ pub fn principal_root(n: f64, nth_root: usize) -> Complex64 {
             }
         })
         .unwrap_or_default()
+}
+
+fn round(to_round: f64, precision: i32) -> f64 {
+    (to_round * (10.0f64.powi(precision))).round() / (10.0f64.powi(precision))
 }
 
 pub mod prelude {

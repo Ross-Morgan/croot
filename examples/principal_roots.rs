e94d49c77ade0cbdbcd4e0c05aad1726aa6b9bca
Ross-Morgan <morgan.ross.512@gmail.com>
2023-04-22 22:07:10 +0100

    conciseness

--- examples/principal_roots.rs
+++ examples/principal_roots.rs
@@ -1,10 +1,10 @@
 use croot::prelude::*;
 
 fn main() {
-    let x = 81;
+    let x = 81.0;
 
     // Principal 4th root of 81
-    let principal = principal_root(x as f64, 4);
+    let principal = principal_root(x, 4);
 
     println!("{principal:?}");
 }

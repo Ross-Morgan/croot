f224a704868e3aa16f561ef1f65cc263ba982d02
Ross-Morgan <morgan.ross.512@gmail.com>
2023-04-22 22:38:13 +0100

    add docs for root, update functions

--- src/roots.rs
+++ src/roots.rs
@@ -3,16 +3,39 @@ use core::f64::consts::PI;
 
 use num_complex::Complex64;
 
+///
 pub fn real_root(n: f64, nth_root: usize) -> f64 {
-    round(n.powf(1.0 / (nth_root as f64)), 10)
+    round(n.abs().powf(1.0 / (nth_root as f64)), 12)
 }
 
 /// Returns nth-roots of n
+///
+/// # Panics
+/// Panics if `nth_root == 0`
+///
+/// # Examples
+///
+/// ```
+/// # use num_complex::Complex64;
+///
+/// // Find roots and use precision
+/// // to remove floating-point errors
+/// let roots = root(81.0, 4).precision(10);
+///
+/// assert!(roots.contains(Complex64::new(3.0, 0.0)));
+/// assert!(roots.contains(Complex64::new(0.0, 3.0)));
+/// assert!(roots.contains(Complex64::new(-3.0, 0.0)));
+/// assert!(roots.contains(Complex64::new(0.0, -3.0)));
+/// ```
 pub fn root(n: f64, nth_root: usize) -> Vec<Complex64> {
-    let real = real_root(n, nth_root);
+    if nth_root == 0 { panic!("Root cannot be zero"); }
+
+    // Part to add to theta for negative roots
+    let add_part = (1.0 - n.signum()) / 2.0;
+    let real = real_root(n, nth_root) * n.signum();
 
     (0..nth_root).map(|idx| {
-        let theta = 2.0 * (idx as f64) / (nth_root as f64) * PI;
+        let theta = 2.0 * (idx as f64 + add_part) / (nth_root as f64) * PI;
         Complex64::from_polar(real, theta)
     }).collect()
 }
@@ -25,23 +48,42 @@ pub fn roots_of_unity(nth_root: usize) -> Vec<Complex64> {
 /// Returns the principal root of n
 ///
 /// The principal root is that with the largest real component
+///
+/// If real components of 2 roots are equal,
+/// the root with the largest complex component is returned
 pub fn principal_root(n: f64, nth_root: usize) -> Complex64 {
-    root(n, nth_root)
-        .into_iter()
-        .reduce(|lhs, rhs| {
-            if lhs.re >= rhs.re {
-                lhs
-            } else {
-                rhs
-            }
-        })
-        .unwrap_or_default()
+    if n == 0.0 {
+        Complex64::default()
+    } else {
+        root(n, nth_root)
+            .into_iter()
+            .reduce(|lhs, rhs| {
+                if lhs.re >= rhs.re {
+                    lhs
+                } else if lhs.re == rhs.re {
+                    if lhs.im >= rhs.im {
+                        lhs
+                    } else {
+                        rhs
+                    }
+                } else {
+                    rhs
+                }
+            })
+            .expect("root function returned no roots")
+    }
 }
 
+/// Round a number to a specified number of decimal places
+///
+/// # Panics
+/// Will panic if to_round * 10^precision > f64::MAX
+///
+/// TODO: Experiment with rounding using logarithms
 fn round(to_round: f64, precision: i32) -> f64 {
     (to_round * (10.0f64.powi(precision))).round() / (10.0f64.powi(precision))
 }
 
 pub mod prelude {
-    pub use super::{principal_root, root, roots_of_unity};
+    pub use super::{principal_root, root, roots_of_unity, real_root};
 }

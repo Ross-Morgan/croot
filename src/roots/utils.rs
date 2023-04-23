fdbfd3af3102b74afb2ce23a4609cd050233838f
Ross-Morgan <morgan.ross.512@gmail.com>
2023-04-23 01:17:59 +0100

    add tests for utils

--- src/roots/utils.rs
+++ src/roots/utils.rs
@@ -9,8 +9,47 @@ pub fn radius(n: f64, nth_root: usize) -> f64 {
 ///
 /// # Panics
 /// Will panic if to_round * 10^precision > f64::MAX
-///
-/// TODO: Experiment with rounding using logarithms
 pub(super) fn round(to_round: f64, precision: i32) -> f64 {
-    (to_round * (10.0f64.powi(precision))).round() / (10.0f64.powi(precision))
+    to_round - (to_round % 10.0_f64.powi(-precision))
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn round_works() {
+        let n = 10.3021;
+
+        let a = round(n, 1);
+        let b = round(n, 2);
+        let c = round(n, 3);
+
+        assert_eq!(a, 10.3);
+        assert_eq!(b, 10.30);
+        assert_eq!(c, 10.302);
+    }
+
+    #[test]
+    fn radius_works() {
+        let n1 = 1.0;
+        let n2 = 81.0;
+        let n3 = 6561.0;
+
+        assert_eq!(radius(n1, 4), 1.0);
+        assert_eq!(radius(n2, 4), 3.0);
+        assert_eq!(radius(n3, 4), 9.0);
+    }
+
+    #[test]
+    fn radius_sign_works() {
+        let n1 = 1.0;
+        let n2 = 81.0;
+        let n3 = 6561.0;
+
+        assert_eq!(radius(n1, 4), radius(-n1, 4));
+        assert_eq!(radius(n2, 4), radius(-n2, 4));
+        assert_eq!(radius(n3, 4), radius(-n3, 4));
+    }
 }

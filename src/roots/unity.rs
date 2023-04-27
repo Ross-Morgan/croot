7409ad1ab44f45f059a4f00cd1c476b892c4764b
Ross Morgan <68851224+Ross-Morgan@users.noreply.github.com>
2023-04-27 11:29:11 +0100

    Update unity.rs

--- src/roots/unity.rs
+++ src/roots/unity.rs
@@ -4,7 +4,7 @@ use core::f64::consts::PI;
 use num_complex::Complex64;
 
 pub fn roots_of_unity(nth_root: usize) -> Vec<Complex64> {
-    (0..n)
+    (0..nth_root)
         .map(|k| Complex64::cis(2.0 * (k as f64) * PI / (nth_root as f64)))
         .collect()
 }

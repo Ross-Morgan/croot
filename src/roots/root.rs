e8ccf54cc9666508232ba3f05d01ea885d307c69
Ross Morgan <68851224+Ross-Morgan@users.noreply.github.com>
2023-04-27 10:56:23 +0100

    Update root.rs

--- src/roots/root.rs
+++ src/roots/root.rs
@@ -3,6 +3,7 @@ use core::f64::consts::PI;
 
 use num_complex::Complex64;
 
+use super::unity::roots_of_unity;
 use super::utils::radius;
 
 /// Returns nth-roots of n
@@ -26,7 +27,15 @@ use super::utils::radius;
 /// assert!(roots.contains(&Complex64::new(0.0, -3.0)));
 /// ```
 pub fn root(n: f64, nth_root: usize) -> Vec<Complex64> {
-    complex_root(Complex64::new(n, 0.0), nth_root)
+    if n > 0 {
+        // Multiply each root of unity by radius length
+        roots_of_unity(nth_root).iter_map().map(|c| c * n).collect()
+    } else if n < 0 {
+        //TODO Implement seperately?
+        complex_root(Complex64::new(n, 0.0))
+    } else {
+        vec![0; nth_root]
+    }
 }
 
 
@@ -43,9 +52,8 @@ pub fn complex_root(c: Complex64, nth_root: usize) -> Vec<Complex64> {
     let real = radius(c.norm(), nth_root);
 
     (0..nth_root)
-        .map(|idx| {
+        .map(|idx| {    
             let n_theta = principal_theta + (2.0 * PI * (idx as f64) / (nth_root as f64));
-
             Complex64::cis(n_theta) * real
         })
         .collect()

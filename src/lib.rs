44ea92499702ae9fdee35de82ac0d3ecb6d9638e
Ross Morgan <morgan.ross.512@gmail.com>
2023-05-13 00:21:31 +0100

    deny clippy::all, add to prelude

--- src/lib.rs
+++ src/lib.rs
@@ -3,16 +3,19 @@
 //! For finding *all* nth-roots
 
 #![no_std]
-#![forbid(unsafe_code, clippy::unwrap_used)]
+#![forbid(unsafe_code)]
+#![deny(clippy::all)]
 
 extern crate alloc;
 
+pub mod approx;
+pub mod period;
 pub mod roots;
-pub mod traits;
 
 pub mod prelude {
     use super::*;
 
-    pub use roots::prelude::*;
-    pub use traits::prelude::*;
+    pub use approx::Approx;
+    pub use period::Period;
+    pub use roots::{complex_root, principal_root, root};
 }

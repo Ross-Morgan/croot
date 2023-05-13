7504a5d2efc4579b00a0c7738d7d381e27f9291f
Ross Morgan <morgan.ross.512@gmail.com>
2023-05-13 19:51:12 +0100

    format + docs

--- src/period.rs
+++ src/period.rs
@@ -1,10 +1,18 @@
 use num_complex::Complex64;
 
-/// Struct wrapping a complex number that represents
+/// Wrapper around a complex number that represents
 /// the multiplicative period of a root
 ///
 /// For a root `R_n` The multiplicative period `P` satisfies the equation:
 /// `R_n+1 = R_n * P`
+///
+/// The multiplicative period can be calculated with the following formula:
+///
+/// # Calculation
+///
+/// `e^(2iπz* / |z|²)`
+///
+/// where `z*` is the complex conjugate and `|z|` is the magnitude.
 ///
 /// # Examples
 /// ```rust
@@ -19,14 +27,24 @@ use num_complex::Complex64;
 /// let root_2 = (root * period.into_inner()).powc(Complex64::new(3.0, 4.0)).approx(5);
 ///
 /// assert_eq!(root_1, root_2);
-///
 /// ```
 #[derive(Copy, Clone, Debug, Default, PartialEq)]
 pub struct Period(pub(crate) Complex64);
 
 impl Period {
-    pub const fn new(c: Complex64) -> Self { Self(c) }
-    pub const fn into_inner(self) -> Complex64 { self.0 }
+    pub const fn new(c: Complex64) -> Self {
+        Self(c)
+    }
 
-    pub fn nth(&self, n: i32) -> Complex64 { self.0 * (n as f64) }
+    pub const fn raw(self) -> Complex64 {
+        self.0
+    }
+
+    pub const fn into_inner(self) -> Complex64 {
+        self.raw()
+    }
+
+    pub fn nth(&self, n: i32) -> Complex64 {
+        self.0 * (n as f64)
+    }
 }
